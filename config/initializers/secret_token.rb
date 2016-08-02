9faef40972c80d7e8e1b8444a0847c6254fd4755
Bao Luong <Bao@Baos-MacBook-Pro.local>
2016-08-02 12:01:08 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FunF::Application.config.secret_key_base = '0d6b507910e5a214c0ba52d574480e55c3393df529900c73a4c7fbe3d834f77b8173f3c4808002d45108ef424339a28695f970878db3311faf7455f8a514c9e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FunF::Application.config.session_store :cookie_store, key: '_FunF_session'
