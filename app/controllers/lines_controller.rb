21813d09db0b5903c3ceb8d7dab9be8aa375b3cf
Bao Luong <Bao@Baos-MacBook-Pro.local>
2016-08-09 12:43:41 -0400

    End Story Logic Added. BeforeSave and RandomStory Broken.

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,5 +1,4 @@
 class LinesController < ApplicationController
-
 	def new
 		@line = Line.new
 	end
@@ -7,10 +6,26 @@ class LinesController < ApplicationController
 	def create
 		@line = Line.create(line_params)
 		if @line.save
-     	    redirect_to controller: "stories", action: "show", id: @line.story_id
-    	else
-        	redirect_to controller: "stories", action: "show", id: @line.story_id
-    	end
+			## creates new story when a story ends
+			if @line.story.lines.length == @line.story.max
+				story1 = Story.create(start_f:  [true, false].sample,
+				          			  max:      rand(10..20))
+
+				Line.create(story: 	  story1,
+	            			text:     PromptName.order("RANDOM()").first.name + " " + PromptAction.order("RANDOM()").first.action + " " + PromptLocation.order("RANDOM()").first.location,
+				            is_f:     story1.start_f,
+						    report:   0)
+
+				flash[:success] = "The End."	
+	     	    redirect_to controller: "stories", action: "show", id: @line.story_id
+	     	else
+	     		flash[:success] = "Thank you for contributing."
+	     		redirect_to controller: "stories", action: "show", id: @line.story_id
+	     	end
+	    else
+	    	flash[:success] = "Error."
+	       	redirect_to controller: "stories", action: "show", id: @line.story_id, alert: "Error."
+	    end
 	end
 
 	private
--- app/models/line.rb
+++ app/models/line.rb
@@ -1,4 +1,15 @@
 class Line < ActiveRecord::Base
 	belongs_to :story
 	validates :text, presence: true
+	before_save :check_exists
+
+
+	/# def check_exists
+		## This is broken.
+		if Line.find_by(id: self.id) == self.id
+			false
+		else
+			true
+		end
+	end#/
 end
